7f8e7c505b2d2ea6af505c1fd04ded2edab5daf6
Pontus Stjernström <pontus@wmyl.se>
2019-10-15 16:04:31 +0200

    Add some scene config?

---
+++ src/GameScene.ts
@@ -0,0 +1,26 @@
+import 'phaser';
+
+class GameScene extends Phaser.Scene {
+
+    constructor() {
+        super({ key: "GameScene" });
+    }
+
+    init(params): void {
+        // TODO
+    }
+
+    preload(): void {
+        // TODO
+    }
+
+    create(): void {
+        // TODO
+    }
+
+    update(time): void {
+        // TODO
+    }
+}
+
+export default GameScene;
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import 'phaser';
 import { Types } from 'phaser';
 import StarfallGame from './StarfallGame';
+import GameScene from "./GameScene";
 
 const config: Types.Core.GameConfig = {
     title: 'Shitfall',
     width: 800,
     height: 600,
     parent: 'game',
-    backgroundColor: '#18216D'
+    backgroundColor: '#18216D',
+    scene: [GameScene],
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    },
 };
 
 window.onload = () => new StarfallGame(config);

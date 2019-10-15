5dcecb34b7311003f3d11b264c859d80693f2616
Pontus Stjernström <pontus@wmyl.se>
2019-10-15 12:40:17 +0200

    Configure dev http-server

--- src/StarfallGame.ts
+++ src/StarfallGame.ts
@@ -1,10 +1,10 @@
-import { Game, Types } from 'phaser';
+import { Game } from 'phaser';
+import { Types } from 'phaser';
 
 class StarfallGame extends Game {
     constructor(config: Types.Core.GameConfig) {
         super(config);
     }
-
 }
 
 export default StarfallGame;
